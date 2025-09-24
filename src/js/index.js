b04dd891f1c87c83c4b0e471c3b774d3561fb5fc
gustaeps2004 <gustaeps@gmail.com>
2025-09-23 21:53:37 -0300

    adicionando pop up

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,39 @@
 function validarSujestoes() {
+  console.log(1)
   if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(document.getElementById("email").value)) {
-    alert("O email inserido está inválido!")
+    abrirPopup(false, "O email inserido está inválido!")
     return false
   }
 
   if (/^\s*$/.test(document.getElementById("comentario").value)) {
-    alert("O campo comentário não pode ser vazio!")
+    abrirPopup(false, "O campo comentário não pode ser vazio!")
     return false
   }
 
-  return true
+  abrirPopup(true, "Sujestão enviado com sucesso!")
+  return false
 }
+
+function abrirPopup(sucesso, mensagem) {
+  popup.style.display = "block";
+  console.log(1)
+  const color = sucesso ? "#008eb6" : "#FF0000"
+
+  popup.style.backgroundColor = color
+
+  document.getElementById("mensagem-pop-up").textContent = mensagem
+
+  clearTimeout(timeoutId);
+  timeoutId = setTimeout(() => {
+    popup.style.display = "none";
+  }, 10000);
+}
+
+const popup = document.getElementById("popup");
+const fechar = document.getElementById("fecharPopup");
+let timeoutId;
+
+fechar.addEventListener("click", () => {
+  popup.style.display = "none";
+  clearTimeout(timeoutId);
+});
